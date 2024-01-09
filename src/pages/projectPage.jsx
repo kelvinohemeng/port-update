dadcb8221649950e5532f3c27d1df76ba193aefc
kelvin Ohemeng <93749415+kelvinohemeng@users.noreply.github.com>
2024-01-09 00:56:09 +0000

    clean up on transition

--- src/pages/projectPage.jsx
+++ src/pages/projectPage.jsx
@@ -75,6 +75,12 @@ const ProjectPage = ({ projects, footer }) => {
     });
   }, []);
 
+  //framer motion variants
+  const transitionVariants = {
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 1 } },
+  };
+
   function isOdd(number) {
     return number % 2 !== 0;
   }
@@ -94,10 +100,10 @@ const ProjectPage = ({ projects, footer }) => {
                   All Projects
                 </h1>
                 <div className="pt-[2rem] flex justify-center">
-                  <div className="flex gap-[5px] flex-wrap justify-center max-w-[50%]">
+                  <div className="flex gap-[10px] flex-wrap justify-center max-w-[50%]">
                     <button>
                       <span
-                        className={`px-2 py-0 text-[12px] border-background text-background border-[.1px] rounded-full ${
+                        className={`px-4 py-1 text-[14px] border-background text-background border-[.1px] rounded-full ${
                           selectedCategory === null
                             ? "bg-background text-white"
                             : ""
@@ -115,7 +121,7 @@ const ProjectPage = ({ projects, footer }) => {
                           onClick={() => setSelectedCategory(higherLevel)}
                         >
                           <span
-                            className={`px-2 py-0 text-[12px] border-background text-background border-[.1px] rounded-full ${
+                            className={`px-4 py-1 text-[14px] border-background text-background border-[.1px] rounded-full ${
                               selectedCategory === higherLevel
                                 ? "bg-background text-white"
                                 : ""
@@ -135,92 +141,110 @@ const ProjectPage = ({ projects, footer }) => {
             <Container>
               {" "}
               <div className=" w-full ">
-                <div className=" w-full relative ">
+                <div className=" w-full relative min-h-[40rem]">
                   <div className="relative  h-full flex flex-wrap justify-center gap-8">
                     {" "}
-                    {filteredProjects.map((select, selectIndex) => (
-                      <AnimatePresence key={`c_${selectIndex}`}>
+                    <AnimatePresence>
+                      {filteredProjects.length === 0 ? (
                         <motion.div
-                          initial={{ opacity: 0, x: -50 }}
-                          animate={{ opacity: 1, x: 0 }}
-                          exit={{ opacity: 0, x: 50 }}
+                          key="no_projects"
+                          initial="initial"
+                          animate="animate"
+                          exit="exit"
+                          variants={transitionVariants}
+                          className=""
                         >
-                          {" "}
-                          <Link
-                            target={select.images ? "_parent" : "_blank"}
-                            to={
-                              select.images
-                                ? `/projects/${select.title}`
-                                : select.live
-                            }
-                            className="group w-fit relative "
+                          <p>No projects found under this category</p>
+                        </motion.div>
+                      ) : (
+                        filteredProjects.map((select, selectIndex) => (
+                          <motion.div
+                            key={`c_${selectIndex}`}
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 50 }}
+                            transition={{
+                              duration: 0.5,
+                              delay: selectIndex * 0.1,
+                            }}
                           >
-                            <div className=" max-w-[290px] 2xl:max-w-[350px] h-full min-h-[30rem] bg-background rounded-lg flex flex-col ">
-                              <div className="bg-red-6 h-full  p-5 flex flex-col justify-between items-start gap-4">
-                                <div className="flex gap-2 flex-wrap">
-                                  {select.category
-                                    .split("|")
-                                    .map((cat, index) => (
-                                      <span
-                                        className=" px-2 py-0 text-[12px] border-white text-white border-[.1px] rounded-full"
-                                        key={index}
-                                      >
-                                        {cat}
-                                      </span>
-                                    ))}
-                                </div>
-                                <div className=" space-y-5">
-                                  <img
-                                    src={select.logo}
-                                    alt=""
-                                    className="w-[50px]"
-                                  />
+                            {" "}
+                            <Link
+                              target={select.images ? "_parent" : "_blank"}
+                              to={
+                                select.images
+                                  ? `/projects/${select.title}`
+                                  : select.live
+                              }
+                              className="group w-fit relative "
+                            >
+                              <div className=" max-w-[290px] 2xl:max-w-[350px] h-full min-h-[30rem] bg-background rounded-lg flex flex-col ">
+                                <div className="bg-red-6 h-full  p-5 flex flex-col justify-between items-start gap-4">
+                                  <div className="flex gap-2 flex-wrap">
+                                    {select.category
+                                      .split("|")
+                                      .map((cat, index) => (
+                                        <span
+                                          className=" px-2 py-0 text-[12px] border-white text-white border-[.1px] rounded-full"
+                                          key={index}
+                                        >
+                                          {cat}
+                                        </span>
+                                      ))}
+                                  </div>
+                                  <div className=" space-y-5">
+                                    <img
+                                      src={select.logo}
+                                      alt=""
+                                      className="w-[50px]"
+                                    />
 
-                                  <h3
-                                    className={
-                                      " text-[24px] leading-[130%] p-font text-white"
-                                    }
-                                  >
-                                    {select.title}
-                                  </h3>
-                                  <div>
-                                    {select.description && (
-                                      <span className="text-[18px] text-white text-opacity-70 line-clamp-3">
-                                        {" "}
-                                        {select.description.split("\n\n")[0]}
-                                      </span>
-                                    )}
+                                    <h3
+                                      className={
+                                        " text-[24px] leading-[130%] p-font text-white"
+                                      }
+                                    >
+                                      {select.title}
+                                    </h3>
+                                    <div>
+                                      {select.description && (
+                                        <span className="text-[18px] text-white text-opacity-70 line-clamp-3">
+                                          {" "}
+                                          {select.description.split("\n\n")[0]}
+                                        </span>
+                                      )}
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className=" border-base-100 border-y border-opacity-40 px-5 py-5 flex justify-between items-center text-white">
-                                <div className="flex items-center">
-                                  <span>
-                                    {select.images
-                                      ? "Case Study"
-                                      : "Live project"}
-                                  </span>
+                                <div className=" border-base-100 border-y border-opacity-40 px-5 py-5 flex justify-between items-center text-white">
+                                  <div className="flex items-center">
+                                    <span>
+                                      {select.images
+                                        ? "Case Study"
+                                        : "Live project"}
+                                    </span>
+                                  </div>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    fill="white"
+                                    viewBox="0 0 256 256"
+                                  >
+                                    <path d="M220,104a4,4,0,0,1-8,0V49.66l-73.16,73.17a4,4,0,0,1-5.66-5.66L206.34,44H152a4,4,0,0,1,0-8h64a4,4,0,0,1,4,4Zm-36,28a4,4,0,0,0-4,4v72a4,4,0,0,1-4,4H48a4,4,0,0,1-4-4V80a4,4,0,0,1,4-4h72a4,4,0,0,0,0-8H48A12,12,0,0,0,36,80V208a12,12,0,0,0,12,12H176a12,12,0,0,0,12-12V136A4,4,0,0,0,184,132Z"></path>
+                                  </svg>
                                 </div>
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="32"
-                                  height="32"
-                                  fill="white"
-                                  viewBox="0 0 256 256"
-                                >
-                                  <path d="M220,104a4,4,0,0,1-8,0V49.66l-73.16,73.17a4,4,0,0,1-5.66-5.66L206.34,44H152a4,4,0,0,1,0-8h64a4,4,0,0,1,4,4Zm-36,28a4,4,0,0,0-4,4v72a4,4,0,0,1-4,4H48a4,4,0,0,1-4-4V80a4,4,0,0,1,4-4h72a4,4,0,0,0,0-8H48A12,12,0,0,0,36,80V208a12,12,0,0,0,12,12H176a12,12,0,0,0,12-12V136A4,4,0,0,0,184,132Z"></path>
-                                </svg>
                               </div>
-                            </div>
-                            <img
-                              src={select.imageUrl}
-                              className="hidden md:block absolute z-[999] top-[-15%] right-[-50%] w-[0px] aspect-video object-cover object-center opacity-0 group-hover:opacity-100 group-hover:w-[300px] origin-bottom-right duration-100 rounded-lg"
-                              alt=""
-                            />
-                          </Link>
-                        </motion.div>
-                      </AnimatePresence>
-                    ))}
+                              <img
+                                src={select.imageUrl}
+                                className="hidden md:block absolute z-[999] top-[-15%] right-[-50%] w-[0px] aspect-video object-cover object-center opacity-0 group-hover:opacity-100 group-hover:w-[300px] origin-bottom-right duration-100 rounded-lg"
+                                alt=""
+                              />
+                            </Link>
+                          </motion.div>
+                        ))
+                      )}
+                    </AnimatePresence>
                   </div>
                 </div>
               </div>
